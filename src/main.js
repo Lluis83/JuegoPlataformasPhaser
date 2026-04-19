d51caa1e4e1184ea5c7b655dfe47dc6383fa49be
Lluis83 <92900143+Lluis83@users.noreply.github.com>
2026-04-19 19:51:10 +0200

    Player class

--- src/main.js
+++ src/main.js
@@ -1,27 +1,3 @@
-class Player extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y, texture) {
-        super(scene, x, y, texture);
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
-
-        this.setBounce(0.2);
-        this.setCollideWorldBounds(true);
-    }
-
-    update(cursors) {
-        if (cursors.left.isDown) {
-            this.setVelocityX(-160);
-        } else if (cursors.right.isDown) {
-            this.setVelocityX(160);
-        } else {
-            this.setVelocityX(0);
-        }
-
-        if (cursors.up.isDown && this.body.touching.down) {
-            this.setVelocityY(-330);
-        }
-    }
-}
 
 const config = {
     type: Phaser.AUTO,
